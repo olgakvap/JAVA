e96e2aeaee1d8e7983a68b9c4931b0c486763a8b
olgakvap <90151897+olgakvap@users.noreply.github.com>
2022-09-10 18:26:44 -0500

    HW1 9/10/22

---
+++ src/HW1.java
@@ -0,0 +1,82 @@
+public class HW1 {
+    public static void main(String[] args){
+
+        String line = "******************************************************";
+        System.out.println(line);
+        int number = 0;
+        System.out.println("Task # " + ++number);
+
+        int a = 3;
+        int b = 2;
+        int c = 1;
+        String str = "Поехали!!!";
+
+        System.out.println(a + "\n" + b + "\n" + c + "\n" + str);
+
+        System.out.println(line);
+        System.out.println("Task # " + ++number);
+
+        String dash = "_";
+        int pict = 8;
+        int pict1 = 1;
+
+        System.out.println(dash + dash + dash + dash + pict + pict + pict + pict + pict + pict + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + dash + dash + dash + pict + pict + pict + pict + pict + pict + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + dash + dash + pict + pict + pict + pict + pict + pict + pict + pict + pict + pict
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + dash + pict + pict + pict + pict + pict + pict + pict + pict + pict + pict + pict
+                + pict + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + dash + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict + pict + pict
+                + pict + pict + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict + pict
+                + pict + pict + pict + dash + dash + dash + pict1 + pict1 + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1 + pict + pict
+                + pict + pict + pict + dash + pict1 + pict1 + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1 + pict + pict
+                + pict + pict + pict + dash + pict1 + pict1 + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1 + pict1 + pict
+                + pict + pict + pict + dash + pict1 + pict1 + dash + dash + dash + dash + dash + dash + dash
+                + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1 + pict
+                + pict + pict + pict + dash + pict1 + pict1 + dash + dash + dash + pict1 + pict1 + dash + dash
+                + pict + pict + pict + pict + pict + pict + pict + dash + dash + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1 + pict
+                + pict + pict + pict + dash + dash + pict1 + dash + dash + pict1 + pict1 + dash + dash
+                + pict + pict + pict + pict + pict + pict + pict + pict + pict + pict + pict + dash + dash + dash);
+        System.out.println(dash + pict + pict + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1
+                + pict + pict + pict + dash + dash + dash + dash+ pict1 + pict1 + dash + dash + pict + pict + pict + pict
+                + pict + pict + pict + pict + pict + pict + pict + pict + pict + dash + dash);
+        System.out.println(dash + dash + pict + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1 + pict1
+                + pict + pict + pict + dash + pict1 + pict1 + pict1 + dash + dash + dash + pict + pict + pict + pict
+                + pict + pict + pict + pict + pict + pict + pict + pict + pict + pict + dash + dash);
+        System.out.println(dash + dash + pict + pict + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + pict1
+                + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + dash + dash + pict1 + pict1 + pict1 + pict1 + pict1
+                + pict + pict + pict + pict + pict + pict + pict1 + pict1 + pict1 + pict1 + dash + dash);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        System.out.println(line);
+        System.out.println("Task # " + ++number);
+
+    }
+}
