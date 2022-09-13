4157ca694ac02516b853808cba7df22511c8ee88
olgakvap <90151897+olgakvap@users.noreply.github.com>
2022-09-13 00:24:04 -0500

    HW1 9/13/22 HW1,2,3

---
+++ src/homework/HW2.java
@@ -0,0 +1,26 @@
+package homework;
+
+public class HW2 {
+    public static void main(String[] args){
+
+        String line = "******************************************************";
+        System.out.println(line);
+        int number = 0;
+        System.out.println("Task # " + ++number);
+
+        int a = 11;
+        int b = 15;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a / b);
+        System.out.println(a * b);
+        System.out.println(a % b);
+        System.out.println(a % 2 == 0);
+        System.out.println(b % 2 == 0);
+        System.out.println("\uD83D\uDE00");
+
+        System.out.println(line);
+        System.out.println("Task # " + ++number);
+    }
+}
--- src/lessons/Main.java
+++ src/lessons/Main.java
@@ -1,3 +1,5 @@
+package lessons;
+
 public class Main {
     public static void main(String[] args){
         System.out.println("Hello world!");
